ad4063beb9f54de6b729e922e64349a8143d2b62
mfmaciel50 <mfmaciel50@gmail.com>
2018-07-24 19:50:45 -0300

    projetoGustavo

--- app/views.py
+++ app/views.py
@@ -16,10 +16,10 @@ def teardown_request(exception):
 
 @app.route('/')
 def index():
-	if 'name' in session:
-		return render_template('index.html', usuario = session['name'])
-	else:
-		return render_template('index.html')
+		if 'name' in session:
+			return render_template('index.html', usuario = session['name'])
+		else:
+			return render_template('index.html')
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -29,12 +29,12 @@ def login():
 		email=request.form['e-mail']
 		senha=request.form['senha']
 		cur = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
-		cur.execute("SELECT * FROM usuario") 
+		cur.execute("SELECT * FROM Usuario") 
 		usuarios = cur.fetchall()
 		for usuario in usuarios:
-			if usuario [1] == email and usuario[3] == senha:
+			if usuario [1] == email and usuario[2] == senha:
 				session['name'] = usuario[0] 
-				session['email'] = usuario[1]
+				session['e-mail'] = usuario[1]
 				return redirect(url_for('index'))
 			else:
 				pass 		
@@ -65,6 +65,18 @@ def perfil():
 	usuario = cur.fetchall()
 	return render_template('perfil.html', usuario = usuario)
 
-@app.route('/criar-bolao')
+@app.route('/criar-bolao', methods = ['GET', 'POST'])
 def criar_bolao():
+	if request.method == 'POST':
+		nome = request.form['nome-do-bolao']
+		time1 = request.form['time1']
+		time2 = request.form['time2']
+		valor = request.form['valor']
+		datainicio = request.form['datainicio']
+		datafim = request.form['datafim']
+		descricao = request.form['descricao']
+		cur = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+		cur.execute("INSERT INTO usuario (nome, email, senha) VALUES (%s, %s, %s)", (nome, email, senha))
+		g.db.commit()
+				
 	return render_template('criarbolao.html')
--- bd/bolao.sql
+++ bd/bolao.sql
@@ -1,14 +1,28 @@
 CREATE TABLE Usuario (
-        nome varchar (255);
-        email varchar(255) NOT NULL;
-        descricao text;
-        senha varchar(255) NOT  NULL;
+        nome varchar (255),
+        email varchar(255) NOT NULL,
+        senha varchar(255) NOT  NULL,
         PRIMARY KEY (email)
 );
 
+CREATE TABLE Bolao(
+        time1 varchar (255),
+        time2 varchar (255),
+        id_bolao serial,
+        email_usuario varchar(255),
+        data_inicio date,
+        data_fim date,
+        nome text,
+        descricao text,
+        valor_minimo float,
+        resultado1 varchar (255),
+        resultado2 varchar (255),
+        PRIMARY KEY (id_bolao),
+        FOREIGN KEY (email_usuario) REFERENCES Usuario (email)
+ );
 CREATE TABLE Apostas (
-        tipo varchar(255),
-        classificacao varchar(255),
+        placar1 varchar (255),
+        placar2 varchar (255),
         id_aposta serial,
         valor float,
         bolao_aposta integer,
@@ -17,16 +31,4 @@ CREATE TABLE Apostas (
         FOREIGN KEY (bolao_aposta) REFERENCES Bolao (id_bolao),
         FOREIGN KEY (email_usuario) REFERENCES Usuario (email)
 );
-
-CREATE TABLE Bolao(
-        publico varchar (255),
-        id_bolao serial,
-        email_usuario varchar(255),
-        data_inicio date,
-        data_fim date,
-        nome text,
-        descricao text,
-        valor_minimo float,
-        PRIMARY KEY (id_bolao),
-        FOREIGN KEY (email_usuario) REFERENCES Usuario (email)
-);
+              
